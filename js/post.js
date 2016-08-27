cc7295940a04efd88530184351c2ca5c7e126bd4
陈铖 <azureternite@live.com>
2016-08-28 01:30:13 +0800

    fixed prepend

--- js/post.js
+++ js/post.js
@@ -45,7 +45,6 @@ $.extend(PostEvents.prototype, {
       dataType: 'json'
     }).done(function(data){
       if(data.success){
-        console.log(data);
         $.each(data.data, function(key, value){
           self.$comments.prepend(self.generateCommentHtml(value));
         });
